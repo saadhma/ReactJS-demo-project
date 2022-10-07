b5d1cf0208c6ff7c27f2422b8c8c45940b210b14
saad ahmad <saad.ahmad@devigital.com>
2022-10-07 16:04:16 +0500

    users list api integration with axios and fetch method

--- src/App.js
+++ src/App.js
@@ -1,95 +1,77 @@
 import './App.css';
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // mounting => outputting react element into an actual DOM element
   
-  state = {
-    editing: false,
-    id: "",
-    valueDate: "",
-    valueDescription: "",
-    todos: []
-  
-}
+  componentDidMount() {
+    // get data from api using axios
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const userList = res.data;
+        console.log(userList);
+        this.setState({ items: userList });
+      })
 
-  getTime() {
-    const d = new Date();
-    const n = d.getTime();
-    return n;
-  }
+      // get data from api using fetch method
 
-  handleDelete(itemId) {
-    const updatedTodoList = this.state.todos.filter((t) => {
-      return t.id !== itemId
-    });
-    this.setState({ todos: updatedTodoList });
-  }
-
-  addNewTodo = (valueDate, valueDescription) => {
-    if (valueDate && valueDescription) {
-      const temp = [...this.state.todos]
-      temp.push(
-        {
-          id: this.getTime(),
-          valueDate: valueDate,
-          valueDescription: valueDescription,
-        }
-      );
-      this.setState({todos:temp})
-      this.setState({ valueDate: valueDate, valueDescription: valueDescription })
-    } else {
-      alert("Please Add Description & Date");
-    }
-  }
-
-  editItem(todo) {
-    this.setState({
-      id: todo.id,
-      valueDate: todo.valueDate,
-      valueDescription: todo.valueDescription,
-      editing: true
-    });
-  }
-
-  editTodo = (valueDate, valueDescription) => {
-    this.state.todos.map((item) => {
-      if (item.id === this.state.id) {
-        return (
-          item.valueDate = valueDate,
-          item.valueDescription = valueDescription
-        );
-      }
-      return this.state.todos
-    });
-    this.setState({ valueDate: valueDate, valueDescription: valueDescription, id: "", editing: false })
+    // fetch(
+    //   "https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       items: json,
+    //       DataisLoaded: true
+    //     });
+    //     console.log(json);
+    //   })
   }
 
   render() {
     return (
       <div className="table-outline">
-        {this.state.editing ?
-          <EditTodo editTodoItem={this.editTodo} valueDate={this.state.valueDate} valueDescription={this.state.valueDescription} /> :
-          <AddTodo addTodoItem={this.addNewTodo} />}
         <table className="table">
           <thead>
             <tr>
-              <td>No.</td>
-              <td>Description</td>
-              <td>Date</td>
+              <td>Id.</td>
+              <td>Name</td>
+              <td>Username</td>
+              <td>Email</td>
+              <td>Phone</td>
+              <td>Website</td>
+              <td>City</td>
+              <td>Street</td>
+              <td>ZipCode</td>
+              <td>Suite</td>
+              <td>Company Name</td>
             </tr>
           </thead>
-          {this.state.todos.map((todo, index) => {
+          {this.state.items.map((item, index) => {
             return (
-              <tbody key={todo.id}>
+              <tbody key={item.id}>
                 <tr>
-                  <td>{index + 1}</td>
-                  <td>{todo.valueDescription}</td>
-                  <td>{todo.valueDate}</td>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.website}</td>
+                  <td>{item.address.city}</td>
+                  <td>{item.address.street}</td>
+                  <td>{item.address.zipcode}</td>
+                  <td>{item.address.suite}</td>
+                  <td>{item.company.name}</td>
                   <td style={{ width: 100 }} className="text-center">
-                    <div className="optionbtn">
-                      <button disabled={this.state.editing} onClick={this.editItem.bind(this, todo)}>Edit</button>
-                      <button onClick={this.handleDelete.bind(this, todo.id)}>Delete</button>
-                    </div>
                   </td>
                 </tr>
               </tbody>
@@ -102,102 +84,3 @@ class App extends Component {
 }
 
 export default App;
-
-class AddTodo extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      addDate: "",
-      addDescription: "",
-    }
-  }
-  
-
-  handleChangeDate = (e) => {
-    this.setState({
-      addDate: e.target.value
-    });
-  }
-
-  handleChangeDescription = (e) => {
-    this.setState({
-      addDescription: e.target.value
-    });
-  }
-
-  clearInput = () => {
-    document.getElementById("todoValueDate").value = "";
-    document.getElementById("todoValueDescription").value = "";
-    this.setState({ addDate: "", addDescription: "" });
-  }
-
-  addTodo = () => {
-    this.props.addTodoItem(this.state.addDate, this.state.addDescription);
-    this.clearInput();
-  }
-
-  render() {
-    return (
-      <div className="input-group">
-        <div className="container">
-          <input type="text" id="todoValueDescription" placeholder="Add Description" onChange={this.handleChangeDescription} />
-        </div>
-        <div className="container">
-          <input type="date" id="todoValueDate" placeholder="Add Date" onChange={this.handleChangeDate} />
-          &nbsp;&nbsp;&nbsp;
-          <button onClick={this.addTodo} type="button">Add New ToDo</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class EditTodo extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      editDate: this.props.valueDate,
-      editDescription: this.props.valueDescription,
-    }
-  }
-
-  handleChangeDate = (e) => {
-    this.setState({
-      editDate: e.target.value
-    });
-  }
-
-  handleChangeDescription = (e) => {
-    this.setState({
-      editDescription: e.target.value
-    });
-  }
-
-  clearInput = () => {
-    document.getElementById("todoValueDate").value = "";
-    document.getElementById("todoValueDescription").value = "";
-    this.setState({ editDate: "", editDescription: "" });
-  }
-
-  editTodo = () => {
-    this.props.editTodoItem(this.state.editDate, this.state.editDescription);
-    this.clearInput();
-  }
-
-  render() {
-    return (
-      <div className="input-group">
-        <div className="container">
-          <input type="text" id="todoValueDescription" name='description' placeholder="Add Description" onChange={this.handleChangeDescription} value={this.state.editDescription} />
-        </div>
-        <div className="container">
-          <input type="date" id="todoValueDate" name='date' placeholder="Add Date" onChange={this.handleChangeDate} value={this.state.editDate} />
-          &nbsp;&nbsp;&nbsp;
-          <button onClick={this.editTodo} type="button">Edit ToDo</button>
-        </div>
-      </div>
-    );
-  }
-}
