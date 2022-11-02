a7e90e69fce161c8652294cabc8e12d6354df597
saad ahmad <saad.ahmad@devigital.com>
2022-11-02 17:39:19 +0500

    react rouder dom implemented

--- src/App.js
+++ src/App.js
@@ -1,95 +1,135 @@
-import './App.css';
-import React, { useState } from 'react';
-import { Button, Input, Table } from './components';
+// BASIC ROUTING
+
+import React from "react";
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
-
-  const [todos, setTodosList] = useState([]);
-  const [editing, setEditing] = useState(false);
-  const [valueId, setValueId] = useState("");
-  const [valueDate, setValueDate] = useState("");
-  const [valueDescription, setValueDescription] = useState("");
-
-  const handleChangeDate = (event) => {
-    const date = event.target.value;
-    setValueDate(date);
-  }
-
-  const handleChangeDescription = (event) => {
-    const description = event.target.value;
-    setValueDescription(description);
-  }
-
-  function clearInput() {
-    setValueDescription("");
-    setValueDate("");
-  }
-
-  function addTodo(date, description) {
-    if (date !== "" && description !== "") {
-      let todoItem = {
-        id: Math.random(),
-        date: date,
-        description: description
-      }
-      setTodosList((todosData) => [...todosData, todoItem]);
-      clearInput();
-    } else {
-      alert("Please add Date and Description");
-    }
-  }
-
-  function editTodo(id, date, description) {
-    if (date !== "" && description !== "") {
-      setTodosList(todos => todos.filter((item) => {
-        if (item.id === id) {
-          return (
-            item.date = date,
-            item.description = description
-          );
-        }
-        return todos;
-      }));
-      setEditing(false);
-    } else {
-      alert("Please add Date and Description");
-    }
-    clearInput();
-  }
-
-  const handleEnableEditing = (todoItem) => {
-    setEditing(true);
-    setValueId(todoItem.id);
-    setValueDate(todoItem.date);
-    setValueDescription(todoItem.description);
-  }
-
-  const handleDeleteTodo = (id) => {
-    setTodosList(todoList => todoList.filter((item) => item.id !== id));
-  };
-
   return (
-    <div className="input-group">
-      <div className="container">
-        <Input type={"text"} placeholder={"Add Description"} onChange={handleChangeDescription} value={valueDescription} />
-      </div>
-      <div className="container">
-        <Input type={"date"} placeholder={"Add Date"} onChange={handleChangeDate} value={valueDate} />
-        &nbsp;&nbsp;&nbsp;
-        {!editing ?
-          <Button
-            onClick={() => addTodo(valueDate, valueDescription)}
-            text="Add New ToDo"
-            className={"btn-small"}
-          /> :
-          <Button
-            onClick={() => editTodo(valueId, valueDate, valueDescription)}
-            text="Edit ToDo"
-            className={"btn-small"}
-          />
-        }
-      </div>
-      <Table todos={todos} handleDeleteTodo={(id) => handleDeleteTodo(id)} handleEnableEditing={(todoItem) => handleEnableEditing(todoItem)} />
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+// NESTED ROUTING
+
+// import React from "react";
+// import {
+//   Routes,
+//   Route,
+//   Link,
+//   useParams,
+//   useMatch
+// } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//         <li>
+//           <Link to="/topics">Topics</Link>
+//         </li>
+//       </ul>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/topics" element={<Topics />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Topics() {
+//   let { path, url } = useMatch();
+  
+//   return (
+//     <div>
+//       <h2>Topics</h2>
+
+//       <h2>Topics</h2>
+//       <ul>
+//         <li>
+//           <Link to={`${url}/rendering`}>Rendering with React</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/components`}>Components</Link>
+//         </li>
+//         <li>
+//           <Link to={`${url}/props-v-state`}>Props v. State</Link>
+//         </li>
+//       </ul>
+
+//       <Routes>
+//         <Route exact path={path}>
+//           <h3>Please select a topic.</h3>
+//         </Route>
+//         <Route path={`${path}/:topicId`}>
+//           <Topic />
+//         </Route>
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// function Topic() {
+//   // The <Route> that rendered this component has a
+//   // path of `/topics/:topicId`. The `:topicId` portion
+//   // of the URL indicates a placeholder that we can
+//   // get from `useParams()`.
+//   let { topicId } = useParams();
+
+//   return (
+//     <div>
+//       <h3>{topicId}</h3>
+//     </div>
+//   );
+// }
